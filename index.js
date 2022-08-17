e7edde013c74a20fefc28fd086d30e964a194757
Handsell <90242087+Bquanganh@users.noreply.github.com>
2022-08-17 11:34:11 +0700

    fix cors them

--- index.js
+++ index.js
@@ -37,14 +37,10 @@ app.post("/api/upload",upload.single("file"),(req,res)=>{
     res.status(200).json("File has been uploaded");
 });
 // Add Access Control Allow Origin headers
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
-    next();
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
   });
 
 
